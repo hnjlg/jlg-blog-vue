6d646c2d0ba46f6d667770b75eeca0a8e17d9379
Liu Jie <1765659336@qq.com>
2023-11-08 17:52:27 +0800

    feat: eslint+prettier统一代码风格，lint-stag对提交的代码做eslint+prettier校验

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-				port: 10100,
-				host: '0.0.0.0',
+	plugins: [
+		vue(),
+		eslintPlugin({
+			include: ['src/**/*.ts', 'src/**/*.vue', 'src/*.ts', 'src/*.vue'],
+		}),
+	],
+	server: {
+		port: 10100,
+		host: '0.0.0.0',
 	},
-})
+});
