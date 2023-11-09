671ec54fde30ca1890a76847dd6abcf21ec03894
Liu Jie <1765659336@qq.com>
2023-11-09 10:54:39 +0800

    feat: js动画库gasp

--- src/views/about/index.vue
+++ src/views/about/index.vue
@@ -1,12 +1,16 @@
 <template>
-	<div>about</div>
+	<div class="about-container">about</div>
 </template>
 
 <script setup lang="ts">
+import { gsap } from 'gsap';
+
 defineOptions({
 	name: 'TestAbout',
 });
 onMounted(() => {
+	console.log(gsap, 'gsap');
+	gsap.to('.about-container', { duration: 1, x: 100 });
 	console.log('TestAbout');
 });
 </script>
