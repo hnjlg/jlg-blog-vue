5021248a8c8d7d565f0dc2bb9e833fa1eb98d3b5
xiongyangyang <1171105495@qq.com>
2024-01-22 15:49:36 +0800

    fix: 修改了一下所有用户的表格字段距离

--- src/views/blog-backend/UserManagement/hooks/useTable.ts
+++ src/views/blog-backend/UserManagement/hooks/useTable.ts
@@ -3,17 +3,22 @@ export default function () {
 		{
 			title: '用户id',
 			field: 'id',
-			minWidth: 300,
+			minWidth: 200,
 		},
 		{
 			title: '用户名',
 			field: 'user_name',
-			minWidth: 300,
+			minWidth: 200,
+		},
+		{
+			title: '用户类型名称',
+			field: 'standing_name',
+			minWidth: 200,
 		},
 		{
 			title: '用户密码',
 			field: 'pass_word',
-			minWidth: 300,
+			minWidth: 400,
 		},
 		// {
 		// 	title: '用户Code',
@@ -25,11 +30,7 @@ export default function () {
 		// 	field: 'user_standing_id',
 		// 	minWidth: 300,
 		// },
-		{
-			title: '用户类型名称',
-			field: 'standing_name',
-			minWidth: 300,
-		},
+
 		// {
 		// 	title: '用户类型值',
 		// 	field: 'standing_value',
